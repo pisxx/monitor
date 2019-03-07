5d771d9dc470e2ac945177ee2c33f641cb06a991
pisxx <piotrek.slawek@gmail.com>
2019-03-07 16:20:32 +0100

    Added dynamodb and sqs utils package

---
+++ services/utils/dynamodb.go
@@ -0,0 +1,72 @@
+package aws
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type Item struct {
+	Hostname string `json:"hostname"`
+	ID       string `json:"id"`
+	IP       string `json:"ip"`
+	OS       string `json:"os"`
+}
+
+func GetAgents(dbname string) string {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	if err != nil {
+		fmt.Println("Got error creating session:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	// Create DynamoDB client
+	svcDynamo := dynamodb.New(sess)
+
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		TableName: aws.String(dbname),
+	}
+
+	// Make the DynamoDB Query API call
+	result, err := svcDynamo.Scan(params)
+
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		os.Exit(1)
+	}
+
+	// fmt.Print(result)
+
+	var listOfAgents []string
+
+	for _, i := range result.Items {
+		item := Item{}
+
+		err = dynamodbattribute.UnmarshalMap(i, &item)
+		if err != nil {
+			fmt.Println("Got error unmarshalling:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		// fmt.Println(item.Hostname)
+		// fmt.Println(item.IP)
+		listOfAgents = append(listOfAgents, item.IP)
+
+	}
+	// fmt.Print(strings.Join(listOfAgents, ","))
+	listOfAgentsJoined := strings.Join(listOfAgents, ",")
+	return listOfAgentsJoined
+}
