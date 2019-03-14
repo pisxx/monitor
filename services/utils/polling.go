20424af5b39516040085d4ebfd09034824cd10c1
pisxx <piotrek.slawek@gmail.com>
2019-03-14 23:07:38 +0100

    Added type for metricsMap

--- services/utils/polling.go
+++ services/utils/polling.go
@@ -7,20 +7,24 @@ import (
 	"net/http"
 )
 
-func PollMetrics(qURL string, agents []string) {
+type metricsMap map[string]string
+
+func PollMetrics(qURL string, agents []string) []metricsMap {
 	// Poll from agents
 	// var metricsSlice []utils.MetricsStruct
 	fmt.Println()
 	// type M map[string]string
-	metricsMapList := make([]map[string]string, 3)
+	metricsMapList := make([]metricsMap, 3)
 	// var metricsMap M
-	metricsMap := make(map[string]string)
+	// metricsMap := make(map[string]string)
+	metricsMap := make(metricsMap)
 	for _, i := range agents {
 		var metrics MetricsStruct
 		fmt.Printf("Polling metrics from %s\n", i)
 		resp, err := http.Get("http://" + i)
 		if err != nil {
-			log.Fatal("Unable to poll metrics")
+			log.Print("Unable to poll metrics")
+			return nil
 		}
 		defer resp.Body.Close()
 
@@ -37,6 +41,6 @@ func PollMetrics(qURL string, agents []string) {
 		fmt.Println()
 	}
 	// fmt.Println(message)
-	fmt.Println("Metrics polled, deleting poll request from SQS")
-	// return metricsMapList
+	// fmt.Fprint(w, "Metrics polled, deleting poll request from SQS")
+	return metricsMapList
 }
