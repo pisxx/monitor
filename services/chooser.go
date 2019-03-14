9d4278bce891e6720a946efbee948b17954a925f
pisxx <piotrek.slawek@gmail.com>
2019-03-14 15:35:22 +0100

    Added API to chooser service

--- services/chooser.go
+++ services/chooser.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/pisxx/monitor/services/utils"
 )
@@ -11,8 +14,21 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/choose", choose)
+	log.Fatal(http.ListenAndServe("0:9001", nil))
+	// go forever()
+	// select {}
+}
+
+func choose(w http.ResponseWriter, req *http.Request) {
+	// for {
 	agents := utils.GetAgents("agents_hostname")
 	log.Printf("List of agents to send %s", agents)
+	fmt.Fprintf(w, "List of agents to send %s\n", agents)
 	sendMessage := utils.SendAgentsList(agents, qURL)
 	log.Printf("Message sent: %s", sendMessage)
+	fmt.Fprintf(w, "Message sent: %v", sendMessage)
+	time.Sleep(5 * time.Second)
+	// fmt.Errorf(format string, a ...interface{})
+	// }
 }
