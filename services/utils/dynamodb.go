e53b9dd52a45492891725c08c80f5c9da93c162b
pisxx <piotrek.slawek@gmail.com>
2019-05-06 22:01:19 +0200

    Resolved issue #9

--- services/utils/dynamodb.go
+++ services/utils/dynamodb.go
@@ -14,9 +14,14 @@ type Item struct {
 	Hostname string `json:"hostname"`
 	ID       string `json:"id"`
 	IP       string `json:"ip"`
+	PublicIP string `json:"public_ip"`
 	OS       string `json:"os"`
 	Port     string `json:"port"`
+	Env      string `json:"env"`
 }
+
+type agentDetailsP map[string]string
+type agentDetailsMap map[string]*agentDetailsP
 
 func DBGetAgents(dbname string, count int) []string {
 	// Initialize a session in us-west-2 that the SDK will use to load
@@ -79,4 +84,79 @@ func DBGetAgents(dbname string, count int) []string {
 	// fmt.Print(strings.Join(listOfAgents, ","))
 	// listOfAgentsJoined := strings.Join(listOfAgents, ",")
 	return agents
+}
+
+func DBGetAgentsDetailed(dbname string, count int) agentDetailsMap {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	if err != nil {
+		fmt.Println("Got error creating session:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	// Create DynamoDB client
+	svcDynamo := dynamodb.New(sess)
+
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		TableName: aws.String(dbname),
+	}
+
+	// Make the DynamoDB Query API call
+	result, err := svcDynamo.Scan(params)
+
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		os.Exit(1)
+	}
+
+	// fmt.Print(result)
+
+	// var agents [][]string
+	// var agents []string
+
+	// agentsList :=
+	agentsDetailsMap := make(agentDetailsMap, 0)
+	for i, res := range result.Items {
+		agentDetails := make(agentDetailsP, 0)
+
+		item := Item{}
+
+		err = dynamodbattribute.UnmarshalMap(res, &item)
+		if err != nil {
+			fmt.Println("Got error unmarshalling:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		agentDetails["hostname"] = item.Hostname
+		agentDetails["port"] = item.Port
+		agentDetails["ip"] = item.IP
+		agentDetails["public_ip"] = item.PublicIP
+		agentDetails["env"] = item.Env
+		agentsDetailsMap[item.Hostname] = &agentDetails
+		// log.Print(agentDetails)
+		// log.Print("-------", agentsMap)
+		// fmt.Println(item.Hostname)
+		// fmt.Println(item.IP)
+		// hostname := item.Hostname
+		// ipPort := item.IP + ":" + item.Port
+		// agents = append(agents, ipPort)
+
+		// agents = append(agents, item.Hostname)
+		// To get only one entry
+		// break
+		if count != -1 {
+			if i == (count - 1) {
+				break
+			}
+		}
+	}
+	// fmt.Print(strings.Join(listOfAgents, ","))
+	// listOfAgentsJoined := strings.Join(listOfAgents, ",")
+	return agentsDetailsMap
 }
--- services/utils/polling.go
+++ services/utils/polling.go
@@ -8,62 +8,79 @@ import (
 	"time"
 )
 
-type metricsMapFor map[string]string
-type metricsMap map[string]interface{}
-type metricsMapList []metricsMap
+type metricsMapForType map[string]string
+type metricsType map[string]interface{}
+type metricsMapType map[string]metricsType
 
-func PollMetrics(agents []string) (metricsMapList, error) {
+// type agentDetails map[string]string
+// type agentDetailsMap map[string]agentDetails
+
+func PollMetrics(agents agentDetailsMap) (metricsMapType, error) {
 	// Poll from agents
 	// var metricsSlice []utils.MetricsStruct
-	fmt.Println()
+	// fmt.Println()
 	// type M map[string]string
-	metricsMapList := make([]metricsMap, 0)
+	metricsMapVar := make(metricsMapType, 0)
 	// var metricsMap M
 	// metricsMap := make(map[string]string)
 	// metricsMap := make(MetricsMap)
-	metricsMapFor := make(metricsMapFor)
+	metricsMapForVar := make(metricsMapForType)
 	timeout := time.Duration(2 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
-	for _, i := range agents {
-		
-		var metrics MetricsStruct
-		metricsMap := make(metricsMap)
-		log.Printf("Polling metrics from %s\n", i)
-		metricsMap["agent"] = i
-		resp, err := client.Get("http://" + i)
+	for _, v := range agents {
+		details := make(agentDetailsP, 0)
+		var metricsStruct MetricsStruct
+		details = *v
+		metricsVar := make(metricsType, 0)
+		// endpoint := details["hostname"]
+		var endpoint string
+		// log.Printf("----- Polling metrics from %s -----\n", endpoint)
+		port := details["port"]
+		if details["env"] == "docker" {
+			endpoint = details["ip"]
+			log.Printf("INFO: Polling metrics from docker: %s:%s\n", endpoint, port)
+		} else if details["env"] == "aws-vm" {
+			endpoint = details["public_ip"]
+			// fmt.Print("------ This is AWS IP : ", details)
+			log.Printf("INFO: Polling metrics from aws vm: %s:%s\n", endpoint, port)
+		}
+		resp, err := client.Get("http://" + endpoint + ":" + port)
 		if err != nil {
-			log.Print("Unable to poll metrics")
-			log.Print(err)
-			metricsMap["status"] = err.Error()
-			metricsMapList = append(metricsMapList, metricsMap)
+			log.Print("ERROR: Unable to poll metrics")
+			log.Print("ERROR: ", err)
+			metricsVar["status"] = err.Error()
+			// metricsMapList = append(metricsMapList, metricsMap)
+			hostname := details["hostname"]
+			metricsMapVar[hostname] = metricsVar
 			continue
 		}
 		defer resp.Body.Close()
 
-		err = json.NewDecoder(resp.Body).Decode(&metrics)
+		err = json.NewDecoder(resp.Body).Decode(&metricsStruct)
 		if err != nil {
 			// log.Fatal("issue")
 			return nil, err
 
 		}
-		for _, v := range metrics.Metrics {
+		for _, v := range metricsStruct.Metrics {
 			fmt.Printf("%s: %s\t", v.Name, v.Value)
-			metricsMapFor[v.Name] = v.Value
+			metricsMapForVar[v.Name] = v.Value
 			// metricsMapList = append(metricsMapList, metricsMap)
 		}
-		metricsMap["metrics"] = metricsMapFor
+		metricsVar["metrics"] = metricsMapForVar
 		fmt.Println()
 		fmt.Println()
+		metricsMapVar[details["hostname"]] = metricsVar
 		// metricsMapList = append(metricsMapList, metricsMap)
 		resp.Body.Close()
 	}
 	// fmt.Println(message)
 	// log.Print("-------- After loop ---------")
 	// log.Print("Length of MetricsMap -> ", len(metricsMapList))
-	log.Print(metricsMapList)
+	// log.Print(metricsMapList)
 	// fmt.Fprint(w, "Metrics polled, deleting poll request from SQS")
 	// return metricsMapList
-	return metricsMapList, nil
+	return metricsMapVar, nil
 }
